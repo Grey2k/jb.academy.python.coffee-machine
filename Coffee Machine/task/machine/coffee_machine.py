392da0ebdae0a90f7e7a13991aec333a72c89f67
Sergey Goppikov <grey2k@gmail.com>
2021-01-06 00:39:58 +0300

    2/6 completed

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,8 +1,24 @@
 # Write your code here
-print('Starting to make a coffee')
-print('Grinding coffee beans')
-print('Boiling water')
-print('Mixing boiled water with crushed coffee beans')
-print('Pouring coffee into the cup')
-print('Pouring some milk into the cup')
-print('Coffee is ready!')
+
+print('Write how many cups of coffee you will need:')
+
+cups = int(input().strip())
+
+print(f'For {cups} cups of coffee you will need:')
+
+
+def calculate_water(cups_qty: int) -> int:
+    return 200 * cups_qty
+
+
+def calculate_milk(cups_qty: int) -> int:
+    return 50 * cups_qty
+
+
+def calculate_beans(cups_qty: int) -> int:
+    return 15 * cups_qty
+
+
+print('{water} ml of water'.format(water=calculate_water(cups)))
+print('{milk} ml of milk'.format(milk=calculate_milk(cups)))
+print('{beans} g of coffee beans'.format(beans=calculate_beans(cups)))
