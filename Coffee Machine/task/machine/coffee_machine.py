5eed2d8bff1d58a860511839a3cb2a3c5767479c
Sergey Goppikov <grey2k@gmail.com>
2021-01-06 01:01:31 +0300

    3/6 completed

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,24 +1,47 @@
 # Write your code here
-
-print('Write how many cups of coffee you will need:')
-
-cups = int(input().strip())
-
-print(f'For {cups} cups of coffee you will need:')
+WATER_RECIPE = 200
+MILK_RECIPE = 50
+BEANS_RECIPE = 15
 
 
 def calculate_water(cups_qty: int) -> int:
-    return 200 * cups_qty
+    return WATER_RECIPE * cups_qty
 
 
 def calculate_milk(cups_qty: int) -> int:
-    return 50 * cups_qty
+    return MILK_RECIPE * cups_qty
 
 
 def calculate_beans(cups_qty: int) -> int:
-    return 15 * cups_qty
+    return BEANS_RECIPE * cups_qty
 
 
-print('{water} ml of water'.format(water=calculate_water(cups)))
-print('{milk} ml of milk'.format(milk=calculate_milk(cups)))
-print('{beans} g of coffee beans'.format(beans=calculate_beans(cups)))
+def calculate_cups(water_qry: int, milk_qty: int, beans_qty: int) -> int:
+    water_cups = water_qry // WATER_RECIPE
+    milk_cups = milk_qty // MILK_RECIPE
+    beans_cups = beans_qty // BEANS_RECIPE
+
+    cups_qty = min(water_cups, milk_cups, beans_cups)
+    return cups_qty
+
+
+print('Write how many ml of water the coffee machine has:')
+water = int(input().strip())
+
+print('Write how many ml of milk the coffee machine has:')
+milk = int(input().strip())
+
+print('Write how many grams of coffee beans the coffee machine has:')
+beans = int(input().strip())
+
+print('Write how many cups of coffee you will need:')
+cups = int(input().strip())
+
+available = calculate_cups(water, milk, beans)
+
+if cups > available:
+    print(f'No, I can make only {available} cups of coffee')
+else:
+    print(
+        'Yes, I can make that amount of coffee{}'.format(
+            ' (and even {extra} more than that)'.format(extra=available - cups) if cups < available else ''))
